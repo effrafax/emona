2e64ec5c4852a72cff9c5f04ded57e7d74459023
Martin Stockhammer <m.stockhammer@web.de>
2011-07-19 21:42:34 +0200

    Added copyright and license text

--- org.emona.edit.ui/src/org/emona/edit/ui/container/WorkspaceProjectsSourceSetState.java
+++ org.emona.edit.ui/src/org/emona/edit/ui/container/WorkspaceProjectsSourceSetState.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ *
+ * Copyright (c) 2011  Martin Stockhammer <m.stockhammer@web.de>
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: 
+ *   Martin Stockhammer - Initial API and implementation
+ *
+ *******************************************************************************/
 package org.emona.edit.ui.container;
 
 import java.util.ArrayList;
