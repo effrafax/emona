548e58611f8b44cc85c82dae9ec863f73fb95340
Martin Stockhammer <m.stockhammer@web.de>
2011-08-17 21:15:42 +0200

    Changed model code

--- org.emona.model/src-gen/org/emona/model/base/impl/HostFailureCriteriaImpl.java
+++ org.emona.model/src-gen/org/emona/model/base/impl/HostFailureCriteriaImpl.java
@@ -7,11 +7,7 @@
  */
 package org.emona.model.base.impl;
 
-import org.eclipse.emf.common.notify.Notification;
-
 import org.eclipse.emf.ecore.EClass;
-
-import org.eclipse.emf.ecore.impl.ENotificationImpl;
 
 import org.emona.model.base.BasePackage;
 import org.emona.model.base.HostFailureCriteria;
--- org.emona.model/src-gen/org/emona/model/base/impl/HostStateImpl.java
+++ org.emona.model/src-gen/org/emona/model/base/impl/HostStateImpl.java
@@ -8,15 +8,15 @@
 package org.emona.model.base.impl;
 
 import java.util.Collection;
+
 import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.common.notify.Notification;
 
 import org.eclipse.emf.ecore.EClass;
 
-import org.eclipse.emf.ecore.impl.ENotificationImpl;
 import org.eclipse.emf.ecore.impl.EObjectImpl;
 
 import org.eclipse.emf.ecore.util.EDataTypeUniqueEList;
+
 import org.emona.model.base.BasePackage;
 import org.emona.model.base.HostState;
 import org.emona.model.base.HostStateValue;
